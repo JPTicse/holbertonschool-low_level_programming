2906d4598d9fc697177b3e488b2b95b0ee9ed0af
JPTicse <jpticse@gmail.com>
2022-06-06 18:28:43 -0700

    fixing betty

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -3,6 +3,8 @@
 /**
  * main - Entry point
  *
+ *
+ *
  * Return: Always 0 (Success)
  */
 
@@ -13,12 +15,11 @@ int main(void)
 	long slong;
 	long long slonglong;
 	float sfloat;
-	
+
 	printf("Size of a char %lu byte(s)\n", sizeof(scharacter));
 	printf("Size of an int: %lu byte(s)\n", sizeof(sinteger));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(slong));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(slonglong));
 	printf("Size of a float: %lu byte(s)\n", sizeof(sfloat));
-	
 	return (0);
 }
