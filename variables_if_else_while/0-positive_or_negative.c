64783c0da4352b7a62ff7ee59f2334727382ec8b
JPTicse <jpticse@gmail.com>
2022-06-07 10:28:38 -0700

    addind Betty and jump line

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 /*
- * main - Entry point
- * Return: Always 0 (Success)
+ *main - Entry point
+ *Return: Always 0 (Success)
  */
 
 int main(void)
@@ -14,11 +14,10 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
-		printf("%i is positive", n);
+		printf("%i is positive\n", n);
 	else if (n < 0)
-		printf("%i is negative", n);
+		printf("%i is negative\n", n);
 	else
-		printf("%i is zero", n);
-	
+		printf("%i is zero\n", n);
 	return (0);
 }
