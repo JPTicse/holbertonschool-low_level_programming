91249d3051bbfc7c3e473c1bb71f5425170730ae
JPTicse <jpticse@gmail.com>
2022-06-12 15:10:15 -0700

    :D

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -7,35 +7,27 @@
 
 int main(void)
 {
-	int a1;
-	int a2;
-	int b1;
-	int b2;
+	int num1, num2;
 
-	for (a1 = '0'; a1 <= '9'; a1++) /*print first two digit combo*/
+	for (num1 = 0; num1 <= 98; num1++)
 	{
-		for (a2 = '0'; a2 <= '9'; a2++)
+		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
-			for (b1 = a1; b1 <= '9'; b1++) /*print second of pair*/
-			{
-				for (b2 = a2 + 1; b2 <= '9'; b2++)
-				{
-					putchar(a1);
-					putchar(a2);
-					putchar(' ');
-					putchar(b1);
-					putchar(b2);
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
 
-					if (a1 != '9' || b1 != '8' || a2 != '9' || b2 != '9')
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-				b1 = '0';
-			}
+			if (num1 == 98 && num2 == 99)
+				continue;
+
+			putchar(',');
+			
+			putchar(' ');
 		}
 	}
+
 	putchar('\n');
 
 	return (0);
