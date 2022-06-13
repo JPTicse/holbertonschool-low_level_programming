389b503764450d3192579e8686d2f3f6bb6eb8f1
JPTicse <jpticse@gmail.com>
2022-06-12 17:06:27 -0700

    Final Task

---
+++ functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the sum of even-valued Fibonacci sequence
+ *        terms not exceeding 4000000.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	unsigned long fib1 = 1, fib2 = 2, sum;
+	int i = 0;
+
+	for (i = 0; i < 98; i++)
+	{
+		sum = fib1 + fib2;
+		printf("%lu, ", sum);
+		fib1 = fib2;
+		fib2 = sum;
+	}
+
+}
