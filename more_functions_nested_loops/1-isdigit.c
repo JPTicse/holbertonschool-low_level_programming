53f6e64e3d9343e4e57e5a075680a449e7a4dace
JPTicse <jpticse@gmail.com>
2022-06-13 13:21:15 -0700

    Task1 and main.h

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _intdigit - code
+ * Description : Evaluate if its a digit
+ *@c:The charaacter
+ * Return:1 if its digit, else 0
+ */
+
+int _intdigit (int c)
+{
+	if (c >= '0' &&  c <= '9')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
