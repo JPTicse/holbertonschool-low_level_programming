0b3f65aa22187de0c1177ad4bae963c6f6389d34
JPTicse <jpticse@gmail.com>
2022-06-16 18:36:56 -0500

    Task 8 and main.h

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - Print n values of the array
+ * @a: The array
+ * @n: The n numbers of index
+ */
+void print_array(int *a, int n)
+{
+/*Print the values of the array*/
+int i = 0;
+	for (i = 0; i < n - 1; i++)
+	{
+		printf("%d, ", a[i]);
+	}
+	printf("%d\n", a[n - 1]);
+}
