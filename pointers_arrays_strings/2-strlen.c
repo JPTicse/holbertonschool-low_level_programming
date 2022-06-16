e885dd055f71032a24db05432d100c722476834b
JPTicse <jpticse@gmail.com>
2022-06-16 12:26:38 -0500

    Task2 and main.h

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - Get the lenght of the string
+ * @s: String
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+	*s = strlen(s);
+	return(0);
+}
