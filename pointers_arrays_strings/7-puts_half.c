9db21b7897a51f9bac002b47fa35661ed781e843
JPTicse <jpticse@gmail.com>
2022-06-16 17:51:53 -0500

    Task 7 and main.h

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * puts_half - Print in reverse a string
+ * Description: Show thr last part of the string
+ * @str: The char
+ */
+void puts_half(char *str)
+{
+/*A loop to know the lenght and print the last half*/
+int i = 0;
+int len = 0;
+	for (len = 0; str[len] != '\0'; len++)
+	{
+	}
+	if (len % 2 != 0)
+	{
+	len = (len - 1) / 2;
+	}
+	for (i = len / 2; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
