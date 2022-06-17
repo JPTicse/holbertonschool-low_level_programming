a59da042b1d2a5032560642510a4c52d4bbcab1e
JPTicse <jpticse@gmail.com>
2022-06-17 03:28:39 -0500

    Task 4 and main.h

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * reverse_array - Concatenate 2 stringsi
+ * @a:  Pointer to Array.
+ * @n: Number of elements.
+ * Description: see variable
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, temp;
+	n -= 1;
+
+	for (i = 0; i < n; i++, n--)
+	{
+		temp = a[i];
+		a[i] = a[n];
+		a[n] = temp;
+	}
+}
+
