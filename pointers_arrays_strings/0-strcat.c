b3674a7913939eed96b5392cfd4667d12ae01462
JPTicse <jpticse@gmail.com>
2022-06-17 01:53:27 -0500

    Adding Task 0 main.h

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strcat - Concatenate 2 stringsi
+ *  * @dest: Pointer to Array.
+ * @src: Number of elements.
+ * Description: see variable
+ * Return: "s1"+"s2"
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *s3 = strcat(dest, src);
+
+	return (s3);
+}
