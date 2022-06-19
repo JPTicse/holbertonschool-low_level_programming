f1f4beb58e40f63b6758110dee8b231f6fbc6ff9
JPTicse <jpticse@gmail.com>
2022-06-19 18:15:54 -0500

    Task 0 amd main.h

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *_memset - modify an memory space
+ * @s: Constant
+ * @b: The value or source
+ * @n: Number of elements.
+ *
+ * Return: s[]
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	for (i = 0; n > 0; i++, n--)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
