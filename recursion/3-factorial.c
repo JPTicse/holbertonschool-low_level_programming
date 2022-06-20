0ad6569efa87a933b59344a7c141f0441d377d5b
JPTicse <jpticse@gmail.com>
2022-06-20 17:57:14 -0500

    Test

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -6,11 +6,11 @@
  * Return: n or factorial
  */
 int factorial(int n)
-{
-	if (n > 1)
-
-	{
+{	
+	if (n < 0)
+		return (-1);
+	else if (n == 0 || n == 1)
+		return (n);
+	else
 		return (n * factorial(n - 1));
-	}
-	return (n);
 }
