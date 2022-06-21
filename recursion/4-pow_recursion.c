65d9fb5634a41de09fbb2a8517778f3511d62ffd
JPTicse <jpticse@gmail.com>
2022-06-20 21:13:39 -0500

    Fixing Bettys

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -7,7 +7,7 @@
  * Return: power or no
  */
 
- int _pow_recursion(int x, int y)
+int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 	{
@@ -19,6 +19,6 @@
 	}
 	else
 	{
-		return x * _pow_recursion(x, y - 1);
-	}	
+	return (x * _pow_recursion(x, y - 1));
+	}
 }
