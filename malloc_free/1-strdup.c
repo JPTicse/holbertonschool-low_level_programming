59af0e1aec332bafac8e8a1d07195c8b184663cc
JPTicse <jpticse@gmail.com>
2022-06-25 15:04:43 -0500

    a

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -10,13 +10,17 @@ char *_strdup(char *str)
 {
 	char *str2;
 	int i, len = 0;
+
 	len = strlen(str);
+
 	if (str == NULL)
 		return (NULL);
-	str2 = malloc(len * sizeof(*str));
+	str2 = malloc(len + 1);
 	if (str2 == NULL)
 		return (NULL);
-	for (i = 0; i <= len; i++)
+	for (i = 1; i < len; i++)
+	{
 		str2[i] = str[i];
+	}
 	return (str2);
 }
