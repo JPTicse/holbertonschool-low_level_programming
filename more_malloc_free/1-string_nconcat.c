7b9342e6dd5f00e51031d935947160bfa4468ab7
JPTicse <jpticse@gmail.com>
2022-06-28 09:42:23 -0500

    Trying nwe formula

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -11,24 +11,31 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j, lens1;
-	int len = 0;
 	char *s3;
+	int i;
+	unsigned int j;
 
-	if (!s1)
+	if (s1 == NULL)
 		s1 = "";
-	if (!s2)
+	if (s2 == NULL)
 		s2 = "";
-	lens1 = 0;
-	while (s1[lens1] != '\0')
-		lens1++;
-	len = lens1 + n + 1;
-	s3 = malloc((sizeof(*s3) * len) + 5);
-	for (i = 0; i < lens1; i++)
+	i = 0;
+	while (s1[i] != '\0')
+		i++;
+	s3 = malloc(sizeof(char) * (i + n + 1));
+	if (s3 == NULL)
+		return (NULL);
+	i = j = 0;
+	while (s1[i] != '\0')
+	{
 		s3[i] = s1[i];
-	lens1++;
-	for (j = 0; j < n && s2[j] != '\0'; j++, i++)
+		i++;
+	}
+	while (j < n && s2[j] != '\0')
+	{
 		s3[i] = s2[j];
+		i++, j++;
+	}
 	s3[i] = '\0';
 	return (s3);
 }
