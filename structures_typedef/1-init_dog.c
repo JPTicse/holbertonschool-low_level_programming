22bbd2ef82e3bf836f12b0ff17d465ac6191a447
JPTicse <jpticse@gmail.com>
2022-06-30 11:02:10 -0500

    Fixing bettys documentation

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -2,8 +2,11 @@
 #include "dog.h"
 /**
 * init_dog - Init struct of dog.
-*
-* Return: Always 0.
+* @d: pointer of the element type dog.
+* @name: name of the dog.
+* @age: age of the dog.
+* @owner: owner of dog.
+* Return: If not exec return.
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
