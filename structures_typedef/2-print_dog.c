8a665365084be1f93caf5086e2375b0a9f0d09e7
JPTicse <jpticse@gmail.com>
2022-06-30 12:03:16 -0500

    Task2

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+* init_dog - Init struct of dog.
+* @d: pointer of the element type dog.
+* @name: name of the dog.
+* @age: age of the dog.
+* @owner: owner of dog.
+* Return: If not exec return.
+*/
+void print_dog(struct dog *d)
+{
+	if (!d)	
+		return;
+	if (d->name)
+		printf("Name: %s\n", d->name);
+	else
+		printf("Name: (nil)\n");
+	if (d->age)
+		printf("Age: %f\n", d->age);
+	else
+		printf("Age: (nil)\n");
+	if (d->owner)
+		printf("Owner: %s\n", d->owner);
+	else
+		printf("Owner: (nil)\n");
+}
