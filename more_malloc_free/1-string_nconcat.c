52bafca76e1440b333aeaead56800589b04292dc
JPTicse <jpticse@gmail.com>
2022-06-30 13:18:36 -0500

    Update

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -22,7 +22,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	i = 0;
 	while (s1[i] != '\0')
 		i++;
-	s3 = malloc(sizeof(char) * (i + n + 1) + 5);
+	s3 = malloc(sizeof(char) * (i + n + 1));
 	if (s3 == NULL)
 		return (NULL);
 	i = j = 0;
