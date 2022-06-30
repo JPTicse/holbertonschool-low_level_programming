aadb216facbb39245f008a21e58a896c4683f468
JPTicse <jpticse@gmail.com>
2022-06-30 12:07:27 -0500

    Fixing Bettys

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 #include "dog.h"
 /**
-* init_dog - Init struct of dog.
+* print_dog - Init struct of dog.
 * @d: pointer of the element type dog.
-* @name: name of the dog.
-* @age: age of the dog.
-* @owner: owner of dog.
 * Return: If not exec return.
 */
 void print_dog(struct dog *d)
 {
-	if (!d)	
+	if (!d)
 		return;
 	if (d->name)
 		printf("Name: %s\n", d->name);
