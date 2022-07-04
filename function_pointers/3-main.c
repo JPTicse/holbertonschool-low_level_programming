e800562a8466677b92ed2cd80860e6ca94a98c37
JPTicse <jpticse@gmail.com>
2022-07-03 22:00:59 -0500

    parethesis

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -27,7 +27,7 @@ int main(int argc, char *argv[])
 	n2 = atoi(argv[3]);
 	f = get_op_func(argv[2]);
 
-	if (!f || argv[2][1] != '\0' )
+	if (!f || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
