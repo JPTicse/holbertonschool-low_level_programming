8d83625067ba0e32b3db716d512708e7f85c1149
JPTicse <jpticse@gmail.com>
2022-07-05 02:08:39 -0500

    {}

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -63,16 +63,23 @@ void print_all(const char * const format, ...)
 			      {'s', print_string},
 			      {'\0', NULL} };
 
+	/* iterate format; if datatype matched, access function via struct */
 	va_start(valist, format);
 	while (format != NULL && format[j] != '\0')
+	{
 		i = 0;
 		while (choice[i].letter != '\0')
+		{
 			if (choice[i].letter == format[j])
+			{
 				printf("%s", separator);
-				choice[i].func(valist);
+				choice[i].func(valist); /*access va_arg later*/
 				separator = ", ";
+			}
 			i++;
+		}
 		j++;
+	}
 	va_end(valist);
 	printf("\n");
 }
