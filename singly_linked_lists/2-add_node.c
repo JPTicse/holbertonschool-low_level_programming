eb0e861c8fd19a5060a1c8b2bd28ab279c2dc3e4
JPTicse <jpticse@gmail.com>
2022-07-17 10:34:41 -0500

    Chaging functions order

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -2,37 +2,13 @@
 #include "lists.h"
 
 /**
- * add_node - function with two arguments
- * @head: double pointer to linked list
- * @str: string pointer
- *
- * Description: adds a new node at the beginning
- * Return: address of new element
- */
-list_t *add_node(list_t **head, const char *str)
-{
-	int count = 0;
-	list_t *temp;
-
-	temp = malloc(sizeof(list_t));
-	if (temp == NULL)
-		return (NULL);
-	temp->str = _strdup(str);
-	while (str[count] != '\0')
-		count++;
-	temp->len = count;
-	temp->next = *head;
-	*head = temp;
-	return (temp);
-}
-
-/**
  * *_strdup - function with one argument
  * @str: string argument
  *
  * Description: returns a pointer to allocated space in memory
  * Return: pointer
  */
+
 char *_strdup(const char *str)
 {
 	int i, j;
@@ -59,4 +35,29 @@ char *_strdup(const char *str)
 	}
 	ptr[j] = '\0';
 	return (ptr);
+}
+
+/**
+ * add_node - function with two arguments
+ * @head: double pointer to linked list
+ * @str: string pointer
+ *
+ * Description: adds a new node at the beginning
+ * Return: address of new element
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	int count = 0;
+	list_t *temp;
+
+	temp = malloc(sizeof(list_t));
+	if (temp == NULL)
+		return (NULL);
+	temp->str = _strdup(str);
+	while (str[count] != '\0')
+		count++;
+	temp->len = count;
+	temp->next = *head;
+	*head = temp;
+	return (temp);
 }
