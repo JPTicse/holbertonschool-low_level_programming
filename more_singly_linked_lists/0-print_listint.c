7533e9620de3a56107137a590dbc469fea87cdcd
JPTicse <jpticse@gmail.com>
2022-07-17 22:12:59 -0500

    Task 0

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -18,24 +18,4 @@ size_t print_listint(const listint_t *h)
 		count++;
 	}
 	return (count);
-}#include "lists.h"
-
-/**
- *  * print_listint - function with one argument
- *   * @h: pointer to struct
- *    *
- *     * Description: prints all the elements of a list
- *      * Return: number of nodes
- *       */
-size_t print_listint(const listint_t *h)
-{
-		int count = 0;
-
-			while (h != NULL)
-			{
-						printf("%d\n", h->n);
-								h = h->next;
-										count++;
-			}
-				return (count);
 }
